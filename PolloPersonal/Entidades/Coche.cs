2be93ae215bf49381e44722990bdf5e52667947b
Andrés Claro Córdoba <32918036+Exagorth@users.noreply.github.com>
2022-06-18 14:47:10 -0300

    Add files via upload

---
+++ PolloPersonal/Entidades/Coche.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolloPersonal.Entidades
+{
+    class Coche
+    {
+        private int Id;
+        private string Marca;
+        private string Modelo;
+        private int Km;
+        private float Precio;
+
+        public Coche(int idCoche,string marcaCoche,string modeloCoche,int kmCoche,float precioCoche)
+        {
+            this.Id = idCoche;
+            this.Marca = marcaCoche;
+            this.Modelo = modeloCoche;
+            this.Km = kmCoche;
+            this.Precio = precioCoche;
+        }
+
+        public Coche() //constructor vacio para setearle valores por teclado
+        {
+
+        }
+
+        public int IdCoche
+        {
+            get => Id;
+            set => Id = value;
+        }
+
+        public string MarcaCoche
+        {
+            get => Marca;
+            set => Marca = value;
+        }
+
+        public string ModeloCoche
+        {
+            get => Modelo;
+            set => Modelo = value;
+        }
+
+        public int KmCoche
+        {
+            get => Km;
+            set => Km = value;
+        }
+
+        public float PrecioCoche
+        {
+            get => Precio;
+            set => Precio = value;
+        }
+
+        public override String ToString()
+        {
+            return "ID_Coche: " + Id + "\nMarca: " + Marca + "\nModelo: " + Modelo + "\nKm_Coche: " + Km + "kms.\n" + "Precio: " + Precio + "$\n";
+        }
+    }
+}
---
+++ PolloPersonal/Program.cs
@@ -0,0 +1,31 @@
+﻿using PolloPersonal.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolloPersonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche auto1 = new Coche(23,"Peugeot","208",0,2370000);
+            Console.WriteLine(auto1);
+
+            Coche auto2 = new Coche();
+
+            auto2.IdCoche = 367;
+            auto2.MarcaCoche = "Wolksvagen";
+            auto2.ModeloCoche = "Gol";
+            auto2.KmCoche = 356455;
+            auto2.PrecioCoche = 3777777.255F;
+
+            string modeloAuto2 = auto2.ModeloCoche;
+            Console.WriteLine(modeloAuto2 + "\n");           
+
+            Console.WriteLine(auto2);
+        }
+    }
+}
