97f00b5cb46650f4552c43d7606df726918b03b8
Exagorth <andres.claro.cordoba@gmail.com>
2022-06-23 18:37:56 -0300

    Ejercicio 2 Listo

---
+++ PolloPersonal/Entidades/Concesionario.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolloPersonal.Entidades
+{
+    class Concesionario
+    {
+        private Coche[] MisCoches;
+        private int Cantidad;
+        private int numeroCoches;
+
+        public Concesionario(int cantidadCoches) //constructor al cual le paso como parametro la dimension del arreglo
+        {
+            this.MisCoches = new Coche[cantidadCoches];
+            Cantidad = cantidadCoches;
+            numeroCoches = 0;                       
+        }
+
+        public void AgregarCoche(Coche coche)
+        //verifico que el objeto coche no este vacio y que el numero de conche que voy a meter no supere la dimension del array.
+        {
+            if (coche != null && numeroCoches < MisCoches.Length)
+            {
+                for (int i = 0; i < MisCoches.Length; i++)
+                {
+                    if (MisCoches[i] == null)
+                    {
+                        MisCoches[i] = coche;
+                        numeroCoches++;
+                        break;
+                    }
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Concesionario lleno!");
+            }
+        }
+        public void mostrarCoches()
+        {
+            for (int i = 0; i < MisCoches.Length; i++)
+            {
+                Console.WriteLine(MisCoches[i]);
+            }
+        }
+
+        public void VaciarConches() 
+        {
+            this.MisCoches = new Coche[Cantidad];
+            numeroCoches = 0;
+        }
+
+        public void eliminarCoche(Coche coche)
+        {
+            if (coche != null && numeroCoches != 0) 
+            {
+                int posicion = -1;
+                
+                for(int i = 0; i < numeroCoches; i++)
+                {
+                    if(coche.IdCoche == MisCoches[i].IdCoche)
+                    {
+                        posicion = i;
+                        break;
+                    }
+                }
+
+                if (posicion == -1)
+                {
+                    Console.WriteLine("No se encuentra Coche");
+                }
+
+                else
+                {
+                    MisCoches[posicion] = null;
+                    numeroCoches--;
+                }
+            }
+        }
+    }
+}
--- PolloPersonal/Program.cs
+++ PolloPersonal/Program.cs
@@ -12,9 +12,14 @@ namespace PolloPersonal
         static void Main(string[] args)
         {
             Coche auto1 = new Coche(23,"Peugeot","208",0,2370000);            
-            Console.WriteLine(auto1);
-
+            
             Coche auto2 = new Coche();
+
+            Coche auto3 = new Coche();
+
+            Coche auto4 = new Coche();
+
+            Coche auto5 = new Coche();
 
             auto2.IdCoche = 367;
             auto2.MarcaCoche = "Ford";
@@ -22,10 +27,41 @@ namespace PolloPersonal
             auto2.KmCoche = 356455;
             auto2.PrecioCoche = 5777777.255F;
 
-            string modeloAuto2 = auto2.ModeloCoche;
-            Console.WriteLine(modeloAuto2 + "\n");           
+            auto3.IdCoche = 27;
+            auto3.MarcaCoche = "BMW";
+            auto3.ModeloCoche = "Alpha 500";
+            auto3.KmCoche = 120;
+            auto3.PrecioCoche = 10777777.100F;
 
-            Console.WriteLine(auto2);
+            auto4.IdCoche = 702;
+            auto4.MarcaCoche = "Fiat";
+            auto4.ModeloCoche = "Uno";
+            auto4.KmCoche = 0;
+            auto4.PrecioCoche = 2222769.115F;
+
+            auto5.IdCoche = 11;
+            auto5.MarcaCoche = "Alpha Roméo";
+            auto5.ModeloCoche = "A1";
+            auto5.KmCoche = 2013;
+            auto5.PrecioCoche = 10999999.15F;
+
+            Concesionario nuevaConce = new Concesionario(5);
+
+            nuevaConce.AgregarCoche(auto1);
+            nuevaConce.AgregarCoche(auto2);
+            nuevaConce.AgregarCoche(auto3);
+            nuevaConce.AgregarCoche(auto4);
+            nuevaConce.AgregarCoche(auto5);
+            //nuevaConce.mostrarCoches();
+            //Console.ReadLine();
+            nuevaConce.eliminarCoche(auto3);
+            //nuevaConce.mostrarCoches();
+            nuevaConce.AgregarCoche(auto3);
+            nuevaConce.mostrarCoches();
+            Console.ReadLine();
+            nuevaConce.VaciarConches();
+            Console.ReadLine();
+            nuevaConce.mostrarCoches();
         }
     }
 }
